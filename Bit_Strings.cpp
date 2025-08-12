800c632dc2d69efaaebb051ac4df1b2b573a035a
MD. ARIF KHAN <130598988+ArifKhan18@users.noreply.github.com>
2025-08-13 01:21:43 +0600

    fefed

---
+++ Bit_Strings.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll   =  long long;
+using ull  =  unsigned long long;
+using ld   =  long double;
+
+#define endl            '\n'
+#define all(x)          (x).begin(), (x).end()
+#define rall(x)         (x).rbegin(), (x).rend()
+#define point(x)        fixed << setprecision(x)
+#define FastIo          ios_base::sync_with_stdio(false); cin.tie(nullptr)
+#define sz(x)           (int)x.size()
+const ll mod = 1e9 + 7;
+long long binpow(long long a, long long b, long long m) {
+	a %= m;
+	long long res = 1;
+	while (b > 0) {
+		if (b & 1)
+			res = res * a % m;
+		a = a * a % m;
+		b >>= 1;
+	}
+	return res;
+}
+
+const ld pi = acos(-1);
+void Plz_Ac() {
+	ll n; cin >> n;
+	cout << (ll)binpow(2, n, mod) << endl;
+}
+
+int main() {
+	FastIo;
+	int test = 1;
+	//cin >> test;
+	while (test--) {
+		Plz_Ac();
+	}
+	return 0;
+}
